cd5301fb9dfbce1e305827caaf266a58ffdf1cd6
Rory Murray <rory.christian.murray@gmail.com>
2019-08-21 16:34:09 -0400

    Create givenPostACourse test in CourseControllerIntegrationTest

--- school/src/test/java/com/lambdaschool/school/controller/CourseControllerIntegrationTest.java
+++ school/src/test/java/com/lambdaschool/school/controller/CourseControllerIntegrationTest.java
@@ -1,5 +1,8 @@
 package com.lambdaschool.school.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Instructor;
 import com.lambdaschool.school.repository.InstructorRepository;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
@@ -9,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.persistence.EntityNotFoundException;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.number.OrderingComparison.lessThan;
@@ -36,6 +41,23 @@ public class CourseControllerIntegrationTest
     public void whenMeasuredResponseTime()
     {
         given().when().get("/courses/courses").then().time(lessThan(5000L));
+    }
+
+    // POST /courses/course/add
+
+    @Test
+    public void givenPostACourse() throws Exception
+    {
+        Instructor i1 = instructorRepository.findById(2L).orElseThrow(() -> new EntityNotFoundException(Long.toString(2L)));
+        Course c1 = new Course("Stonks", i1);
+        c1.setCourseid(100);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String stringCourse1 = mapper.writeValueAsString(c1);
+
+        given().contentType("application/json").body(stringCourse1).when().post("/courses/course/add").then().statusCode(201);
+
+
     }
 
 
--- school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
+++ school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
@@ -54,23 +54,23 @@ class CourseControllerTest
     {
     }
 
-//    @Test
-//    void listAllCourses() throws Exception
-//    {
-//        String apiUrl = "/courses/courses";
-//
-//        // Create mock call
-//        Mockito.when(courseService.findAll()).thenReturn(courseList);
-//
-//        // Create real call
-//
-//        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
-//        MvcResult r = mockMvc.perform(rb).andReturn();
-//        String tr = r.getResponse().getContentAsString();
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//        String er = mapper.writeValueAsString(courseList);
-//
-//        assertEquals("Rest API Returns List", er, tr);
-//    }
+    @Test
+    void listAllCourses() throws Exception
+    {
+        String apiUrl = "/courses/courses";
+
+        // Create mock call
+        Mockito.when(courseService.findAll()).thenReturn(courseList);
+
+        // Create real call
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb).andReturn();
+        String tr = r.getResponse().getContentAsString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(courseList);
+
+        assertEquals("Rest API Returns List", er, tr);
+    }
 }
