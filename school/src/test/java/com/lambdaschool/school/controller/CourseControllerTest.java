b414558087de761aab7180f16914aa4a2cacf59f
Rory Murray <rory.christian.murray@gmail.com>
2019-08-21 16:42:41 -0400

    Create addNewCourse test in CourseControllerTest

--- school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
+++ school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
@@ -2,6 +2,7 @@ package com.lambdaschool.school.controller;
 
 import com.lambdaschool.school.SchoolApplication;
 import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Instructor;
 import com.lambdaschool.school.service.CourseService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
@@ -18,11 +19,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = CourseController.class, secure = false)
@@ -73,4 +77,29 @@ class CourseControllerTest
 
         assertEquals("Rest API Returns List", er, tr);
     }
+
+    @Test
+    public void addNewCourse throws Exception
+    {
+        String apiUrl = "/courses/course/add";
+
+        // create a course to test
+        Instructor i1 = new Instructor("Skyelar");
+        Course c1 = new Course("Cyber Security", i1);
+        c1.setCourseid(42);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String courseString = mapper.writeValueAsString(c1);
+
+        Mockito.when(courseService.save(any(Course.class))).thenReturn(c1);
+
+        // Build mock request
+        RequestBuilder rb = MockMvcRequestBuilders.post(apiUrl).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).content(courseString);
+        mockMvc.perform(rb).andExpect(status().isCreated()).andDo(MockMvcResultHandlers.print());
+
+
+
+
+    }
+
 }
